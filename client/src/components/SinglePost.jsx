7369df419ffd0e13984d3eb9926ec1113a45cae2
Kane Lawless <kanelawless@gmail.com>
2024-06-02 16:38:37 +0100

    added comments to single post and add comment elements

--- client/src/components/SinglePost.jsx
+++ client/src/components/SinglePost.jsx
@@ -16,24 +16,27 @@ export default function SinglePost() {
 
     const [postData, setPostData] = useState()
     const [interested, setInterested] = useState()
+    const [commentInput, setCommentInput] = useState()
     const params = useParams()
 
-    useEffect(() => {
-        async function getData() {
-            try {
-                const { data } = await axios.get(`/api/posts/${params.postId}`)
-                setPostData(data)
-                console.log(data)
+    async function getData() {
+        try {
+            const { data } = await axios.get(`/api/posts/${params.postId}`)
+            setPostData(data)
+            console.log(data)
 
-                if (isLoggedIn()) {
-                    const token = jwtDecode(getToken())
-                    console.log(token)
-                    data.interests.includes(token.user_id) ? setInterested(true) : setInterested(false)
-                }
-            } catch (error) {
-                console.log(error)
+            if (isLoggedIn()) {
+                const token = jwtDecode(getToken())
+                console.log(token)
+                data.interests.includes(token.user_id) ? setInterested(true) : setInterested(false)
             }
+        } catch (error) {
+            console.log(error)
         }
+    }
+
+    useEffect(() => {
+
         getData()
 
     }, [])
@@ -47,6 +50,26 @@ export default function SinglePost() {
             console.log(error)
         }
     }
+
+
+    function handleChange(e) {
+        setCommentInput(e.target.value)
+    }
+
+
+    async function handleSubmit(e) {
+        try {
+            const res = await axios.post('/api/comments/', { text: commentInput, post: params.postId },
+                { headers: { Authorization: `Bearer ${getToken()}` } }
+            )
+            console.log(res)
+            getData()
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
 
     return (
         <Container>
@@ -69,6 +92,27 @@ export default function SinglePost() {
                                 </div>
                             </Card.Body>
                         </Card>
+                        <div>
+                            <div className="mb-3">
+                                <label htmlFor="comment-input" className="form-label"></label>
+                                <textarea value={commentInput} onChange={handleChange} className="form-control mb-3" id="comment-input" rows="3" placeholder="Add a comment.."></textarea>
+                                <div className="d-flex flex-row justify-content-end">
+                                    <button onClick={handleSubmit}>Post Comment</button>
+                                </div>
+                            </div>
+                            {postData.comments.map((comment) => {
+                                return (
+                                    <Card className="mb-2" key={`comment_${comment.id}`}>
+                                        <Card.Title>{comment.owner.username} <small>{timeAgo(comment.created_at)}</small></Card.Title>
+                                        <Card.Body>
+                                            {comment.text}
+                                        </Card.Body>
+                                    </Card>
+                                )
+                            })
+                            }
+
+                        </div>
                     </Col>
                 }
                 <Col></Col>
--- posts/models.py
+++ posts/models.py
@@ -5,13 +5,14 @@ class Post(models.Model):
     title = models.CharField()
     text = models.TextField()
     image = models.CharField(blank=True, null=True)
-    community = models.ForeignKey('communities.Community', related_name='posts', on_delete=models.CASCADE)
+    community = models.ForeignKey('communities.Community', related_name='posts', on_delete=models.CASCADE, blank=True)
     interests = models.ManyToManyField('users.User',related_name='interested_in', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(
         'users.User',
         related_name='posts_created',
-        on_delete=models.CASCADE,blank=True)
+        on_delete=models.CASCADE,
+        blank=True)
     categories = models.ManyToManyField('categories.Category', related_name='posts', blank=True)
     
     def __str__(self):
